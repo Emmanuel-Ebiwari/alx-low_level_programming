b57ad35549bf1d0596a1dbf8245f1e10850a3530
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-08-27 06:13:18 -0700

    minor fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
 * Description: prints Programming is like building a multilingual puzzle
 * Main - Entry point
 * Return: 0
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
