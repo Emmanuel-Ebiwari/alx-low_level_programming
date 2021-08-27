c946ca0244ef4f1160cebf8f912a0e32f386aae9
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-08-27 06:47:18 -0700

    minor fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints "\"Programming is like 
+ * main - Prints "\"Programming is like
  * building a multilingual puzzle"
  * Description: This is a description
- *
  * Return: 0
  */
 int main(void)
