74da944bbbcf73ba78059a7ad8a8e46f8d29d079
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-08-27 07:58:13 -0700

    minor fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Prints "and that piece of art is
- * useful" - Dora Korpar, 2015-10-19\n"
+ * useful\" - Dora Korpar, 2015-10-19\n"
  *
  * Return: 1
  */
 int main(void)
 {
-	write(stderr,"and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n");
+	fwrite(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
