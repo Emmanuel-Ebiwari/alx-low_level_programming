fd9f76a510c28613cc4f938ef3075e7c8a557bcb
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-08-28 13:36:57 -0700

    adding 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints Write a program that prints
+ * the alphabet in lowercase, and then in
+ * uppercase, followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	char spc = '\n';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+        putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(spc);
+
+	return (0);
+}
