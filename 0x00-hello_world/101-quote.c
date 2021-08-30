c136da3e349bcf3f49afc6e153f80a0531edb683
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-08-30 04:40:06 -0700

    adding 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 /**
  * main - Prints "and that piece of art is
- * useful\" - Dora Korpar, 2015-10-19\n"
+ * useful\" - Dora Korpar, 2015-10-19\n" to the
+ * standard error
  *
  * Return: 1
  */
 int main(void)
 {
-	write(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
