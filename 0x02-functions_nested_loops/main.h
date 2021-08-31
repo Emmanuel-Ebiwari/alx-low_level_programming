1607f79138f4c5a909479e86a7472fb93f0e1ce3
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-08-31 10:19:54 -0700

    adding 2-print_alphabet_x10.c file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
 /* _putchar.c function prototype*/
 int _putchar(char);
+/* print_alphabet function proptotype*/
 void print_alphabet(void);
+/* print_alphabet_x10 function proptotype*/
+void print_alphabet_x10(void);
