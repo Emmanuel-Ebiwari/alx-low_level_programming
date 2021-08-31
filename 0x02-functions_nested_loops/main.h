39cb087975d71118260e89e02a5414cebdebefca
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-08-31 11:32:41 -0700

    adding 4-isalpha.c file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,7 @@ int _putchar(char);
 void print_alphabet(void);
 /* print_alphabet_x10 function prototype*/
 void print_alphabet_x10(void);
-/* int _islower(int c) function prototype*/
+/* int _islower function prototype*/
 int _islower(int c);
+/* _isalpha function prototype*/
+int _isalpha(int c);
