cdd995d43a2f8309a1128a9d292f7a923e9d7fc5
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-08-31 21:02:22 -0700

    adding 8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min < 60; min++)
 		{
-		_putchar((min / 10) + '0');
-		_putchar((min % 10) + '0');
-		_putchar(':');
 		_putchar((hr / 10) + '0');
 		_putchar((hr % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
 		_putchar('\n');
 		}
 	}
