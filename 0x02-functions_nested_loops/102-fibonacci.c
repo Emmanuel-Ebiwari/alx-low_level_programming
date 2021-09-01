f50e359960ddbecfa1c2949d4aad9f91f6afd840
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-01 09:39:14 -0700

    adding 101-natural.c file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,15 +8,20 @@
   */
 int main(void)
 {
-	int n1 = 1;
-	int n2 = 2;
-	int sum = n1 + n2;
+	long int n1 = 1;
+	long int n2 = 2;
+	long int sum = n1 + n2;
 	int i;
 
-	printf("%i, %i, ", n1, n2);
+	printf("%lu, %lu, ", n1, n2);
 	for (i = 3; i <= 50; ++i)
 	{
-		printf("%i, ", sum);
+		printf("%lu", sum);
+		if (i < 50)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n1 = n2;
 		n2 = sum;
 		sum = n1 + n2;
