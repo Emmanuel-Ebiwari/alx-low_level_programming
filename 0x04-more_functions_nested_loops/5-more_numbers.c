7e5b02bf70b22766cb11113dd019b56b6fd8499e
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-02 00:22:07 -0700

    adding 5-more_numbers.c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - checks for lowercase character.
+ */
+void more_numbers(void)
+{
+	int j, i = 0;	
+
+	while (i < 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			else
+				_putchar(j + '0');
+		}
+		j = 0;
+		i++;
+	_putchar('\n');
+	}
+}
