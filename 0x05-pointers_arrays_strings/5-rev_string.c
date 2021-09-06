c0aa00a28161f5b12732cfa46417d13ea07eb5e7
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-06 14:11:37 -0700

    adding 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int length;
+	
+	length = _strlen(s);
+
+	_putchar(length + '0');
+
+/*	for (i=length; i >= 0; i--) */
+		
+		
+}
