0f6c829b0b2a54b714187cce9d0beb1ab268edfa
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-10 01:24:52 -0700

    updating 100-rot13.c file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,21 +9,18 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char a1[] = "abcdefghijklm";
-	char a2[] = "nopqrstuvwxyz";
+	char a1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char a2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 13; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == a1[j])
+			{
 				s[i] = a2[j];
-			else if (s[i] == a1[j] - 32)
-				s[i] = a2[j] - 32;
-			else if (s[i] == a2[j])
-				s[i] = a1[j];
-			else if (s[i] == a2[j] - 32)
-				s[i] = a1[j] - 32;
+				break;
+			}
 		}
 	}
 
