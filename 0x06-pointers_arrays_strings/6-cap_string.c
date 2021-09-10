7a8df00d3a27568c86045dbeb1d59f54956acaf4
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-10 05:42:28 -0700

    updating 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,43 +1,5 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string
- *
- * @s: string
- * Return: length
- */
-int _strlen(char *s)
-{
-	int length = 0;
-	int i;
-
-
-	for (i = 0; s[i] != '\0'; i++)
-		length = i + 1;
-	return (length);
-}
-
-/**
- * string_toupper - changes all lowercase letters of
- * a string to uppercase
- *
- * @s: string
- * Return: string
- */
-char *string_toupper(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] >= 97 && s[i] <= 122)
-			s[i] -= 32;
-		i++;
-	}
-	return (s);
-}
-
-/**
  * cap_string - capitalizes all words of a string
  *
  * @s: string
@@ -47,15 +9,31 @@ char *string_toupper(char *s)
 char *cap_string(char *s)
 {
 	int i;
+/*	char d[] = ",\t;\n; .!?\"(){}";*/
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
-			s[i] -= 32;
-		else if (s[i] >= 65 && s[i] <= 90)
-			s[i] -= 0;
-		i++;
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+			continue;
+		}
+		if (s[i] == ' ' || s[i] == '\n')
+		{
+			++i;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] -= 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (s[i] >= 'A' && s[i] <= 'Z')
+				s[i] += 32;
+		}
 	}
+
 	return (s);
 }
