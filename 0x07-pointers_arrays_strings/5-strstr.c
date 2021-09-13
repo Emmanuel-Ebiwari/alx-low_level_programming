8167d088407df5312389cb00377f2383d0eb2578
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-13 15:55:25 -0700

    adding 5-strstr.c file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: pointer to the beginning of the located
+ * substring, or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+			return (haystack);
+		haystack++;
+	}
+	if (!*needle)
+		return (haystack);
+	return (NULL);
+}
