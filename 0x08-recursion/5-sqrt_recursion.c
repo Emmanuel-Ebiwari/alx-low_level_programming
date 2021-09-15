1179dadb355cfbf7a2677d6480e6a96d4cfa4533
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-15 08:49:58 -0700

    updating 5-sqrt_recursion.c file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,10 +3,32 @@
  * _sqrt_recursion - returns the natural square root of a number
  *
  * @n: integer
+ * @i: integer
+ *
+ * Return: the square root of n
+ */
+int isSqrt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+        return (isSqrt(n, i + 1));
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: integer
  *
  * Return: the square root of n
  */
 int _sqrt_recursion(int n)
 {
-	return( _sqrt_recursion((n + n / n) / 2));
+	if (n == 1 || n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (isSqrt(n, 2));
 }
