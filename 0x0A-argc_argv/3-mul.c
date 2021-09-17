ece6d1d19bb6051b6a008072e17c7da11c75cbee
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-17 06:15:36 -0700

    adding 3-mul.c file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,16 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 1;
 
-	if (argc == 3)
+	if (argc != 3)
 	{
+		printf("ERROR\n");
+		return (1);
+	}
+
 	for (i = 1; i < argc; i++)
 		sum *= atoi(argv[i]);
 
-	printf("%d\n", sum);
+	printf("%d\n", atoi(sum));
 	return (0);
 	}
-	printf("ERROR\n");
-	return (1);
 }
