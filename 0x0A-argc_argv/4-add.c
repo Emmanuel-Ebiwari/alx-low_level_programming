f8eb22530f3fe192ccd9999bffe4dfd7962ba3e6
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-19 22:30:57 -0700

    adding 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+		for (i = 1; i < argc; i++)
+		{
+ 			if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += atoi(argv[i]);
+		}
+	argc > 1 ? printf("%d\n", sum) : printf("0\n");
+	return (0);
+}
