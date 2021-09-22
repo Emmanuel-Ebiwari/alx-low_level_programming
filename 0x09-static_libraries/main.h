53629cb8972d7ccbdd6931e82492be876e3ea1e5
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-22 15:05:38 -0700

    updating main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -37,3 +40,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 int _atoi(char *s);
+
+#endif
