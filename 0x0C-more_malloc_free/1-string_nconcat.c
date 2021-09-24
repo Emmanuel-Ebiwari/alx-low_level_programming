11cdd6d80f26c804a00776445f45aab04ea15cf2
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-23 19:25:04 -0700

    updating 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strlen - returns the length of a string
+ *
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	int i;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+		length = i + 1;
+	return (length);
+}
+
+/**
  * string_nconcat - concatenates two strings
  *
  * @s1: first string
@@ -12,27 +29,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len;
+	unsigned int i, j, s1_len, s2_len, bytes;
 	char *ptr;
 
-	len = 0;
-	
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0;s1[i] != '\0'; i++)
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
+
+	if (n > s2_len)
+		n = s2_len;
+
+	bytes = s1_len + (s2_len - n);
+
+	ptr = malloc(sizeof(char) * bytes);
+
+	for (i = 0; i < s1_len; i++)
 	{
-		len++;
+		ptr[i] = s1[i];
 	}
 
-	for (i = 0; s2[i] != '\0' && i < n; i++)
+	for (j = 0; j < n; j++)
 	{
-		s1[len + i] = s2[i];
+		ptr[i++] = s2[j];
 	}
-	s1[len + i] = '\0';
+	ptr[i] = '\0';
 
-	return (s1);
+	return (ptr);
 }
