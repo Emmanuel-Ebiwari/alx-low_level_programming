e32db48efbb4306da1c0faf4ad18a8c949c7e3e6
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-27 17:22:01 +0100

    adding dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct my_struct - Short description
+ * @name: character
+ * @owner: character
+ * @age: float
+ *
+ * Description: struct dog prototype
+ */
 struct dog
 {
 	char *name;
