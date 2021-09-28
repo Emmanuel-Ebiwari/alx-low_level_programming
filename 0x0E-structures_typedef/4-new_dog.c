59efcc0e9a915fd88838692d4e9d7c7186e7d939
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-28 07:13:19 -0700

    minor fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *  * _strlen - returns the length of a string
- *   *
- *    * @s: string
- *     * Return: length
- *      */
+ * _strlen - returns the length of a string
+ *
+ * @s: string
+ * Return: length
+ */
 int _strlen(char *s)
 {
 	int length = 0;
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 		length = i + 1;
 	return (length);
@@ -46,14 +47,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i <= len1; i++)
 		my_dog->name[i] = name[i];
 	my_dog->age = age;
-	my_dog->owner = malloc(len2 * sizeof(my_dog->owner)); 
+	my_dog->owner = malloc(len2 * sizeof(my_dog->owner));
 	if (my_dog->owner == NULL)
 	{
 		free(my_dog->owner);
 		free(my_dog->name);
 		free(my_dog);
 		return (NULL);
-	} 
+	}
 	for (i = 0; i <= len2; i++)
 		my_dog->owner[i] = owner[i];
 	return (my_dog);
