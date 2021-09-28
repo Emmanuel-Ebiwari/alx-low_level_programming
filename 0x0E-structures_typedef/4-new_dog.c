e5154c942fa08737610c70209da84bf2bb89ebbe
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-28 14:23:40 +0100

    minor dix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j, len1 = 0, len2 = 0;
 	dog_t *my_dog;
 
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		len1++;
+	}
+
+	for (i = 0; owner[i] != '\0'; i++)
+        {
+                len2++;
+        }
+
 	my_dog = malloc(sizeof(dog_t));
+	my_dog->name = malloc(len1 * sizeof(my_dog->name));
+	my_dog->owner = malloc(len2 * sizeof(my_dog->owner));
 
 	if (my_dog == NULL)
 		return (NULL);
 
-	my_dog->name = name;
+	for (j = 0; j <= len1; j++)
+		my_dog->name[j] = name[j];
 	my_dog->age = age;
-	my_dog->owner = owner;
+	for (j = 0; j <= len2; j++)
+		my_dog->owner[j] = owner[j];
 
 	return (my_dog);
 }
