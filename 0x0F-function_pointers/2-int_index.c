ad06dcf080c1796c3e512f5f283f4e93a5197739
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-29 14:21:42 -0700

    adding 2-int_index.c file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * int_index - searches for an integer
+ *
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer function
+ *
+ * Return: intgers
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+}
