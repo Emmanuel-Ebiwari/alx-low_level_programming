a8193bcafc58d82a97825462f57ae49d9f55fd8f
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-30 00:05:26 +0100

    adding 2-int_index.c file

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,18 @@
+#ifndef THREE_CALC_H
+#define THREE_CALC_H
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+
+
+#endif /*THREE_CALC_H*/
