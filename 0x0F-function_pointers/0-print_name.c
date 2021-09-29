602871ac2c464b80a0366ed433bed9995ce5894a
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-09-29 20:38:58 +0100

    adding 0-print_name.c file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - prints a name
+ *
+ * @name: string to print
+ * f: pointer function
+ */
+void print_name(char *name, void (*f)(char *s))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+File **1-array_iterator.c** - *Writes a function that prints a name*
