fbe571099ac973eab86fd476f9bf7df2f8924cf2
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-01 21:31:34 +0100

    adding 3-print_all.c file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 			printf("%d", va_arg(ap, int));
 			break;
 		case 'f':
-			printf("%f", va_arg(ap, double*));
+			printf("%f", va_arg(ap, double));
 			break;
 		case 's':
 			printf("%s", va_arg(ap, char*));
