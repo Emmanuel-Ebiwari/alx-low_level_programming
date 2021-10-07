da940748c6b29ed05a21c40f55ae3c665c19b225
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-07 14:06:37 -0700

    updating 100-first.c dilw

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,10 @@
 /**
  * startup_func - executes code before main function
  */
-void startup_func (void) __attribute__ ((constructor));
+void startup_func(void) __attribute__ ((constructor));
 
 void startup_func(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI
+	bore my house upon my back!\n");
 }
