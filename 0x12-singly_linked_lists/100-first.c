ae210a907cedab3adbd68b307263693420d426a6
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-07 22:12:04 +0100

    updating 100-first.c file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void startup_func(void) __attribute__ ((constructor));
 
 void startup_func(void)
 {
-	printf("You're beat! and yet, you must allow,\nI
-	bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
