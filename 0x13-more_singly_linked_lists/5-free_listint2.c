0a9c9257379dd40cca4d7f369f36a1eda8d08243
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-12 21:11:19 +0100

    updating 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (head)
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (*head)
 	{
-		while (*head)
-		{
-			free(*head);
-			*head = (*head)->next;
-		}
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp
 	}
+
+	*head = NULL;
 }
