fc5b596eb8ef3c461213f51db824e20adfb50d04
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-14 23:35:01 +0100

    adding 0-binary_to_uint.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sum = 0, x = 2;
 	int len = strlen(b), i;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
 	for (i = len - 1; i >= 0; i--)
@@ -26,7 +26,7 @@ unsigned int binary_to_uint(const char *b)
 			if (b[i] == '1')
 				sum += 1;
 		}
-		if (i < len - 1)
+		else if (i < len - 1)
 		{
 			if (b[i] == '1')
 			{
