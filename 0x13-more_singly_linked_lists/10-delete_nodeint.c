42399bb3dc9688776fce8f338453f82181efb356
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-14 01:35:33 +0100

    updating 10-delete_nodeint.c file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 //	current = *head;    
-	while (count < index)
+	while (current && count < index)
 	{
 		if (current == NULL || current->next == NULL)
 			return (-1);
