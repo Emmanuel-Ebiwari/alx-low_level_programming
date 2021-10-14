e5cdbc24b18dbb37fc67c34690f19a1e0bcb13e1
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-14 01:08:48 +0100

    updating 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,13 +40,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		if (idx > count)
 			return (NULL);
-<<<<<<< HEAD
 	 }
 	else
-=======
-	}
-	if (idx == 0)
->>>>>>> 78b3ba9317501307bb4949e0ae7d223ca191f86d
 	{
 		new_node->next = *head;
 		*head = new_node;
