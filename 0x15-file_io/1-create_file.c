6b30c5cdfe9f51ec9abf7b1bad4654c125b4c68a
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-18 23:41:05 +0100

    adding 1-create_file.c file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ *
+ * @filename: pointer to a file
+ * @text_content: content of the file being created
+ *
+ * Return: 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		 return (-1);
+
+	if (!text_content)
+		 write(fd, "", 0);
+
+	while (text_content[i])
+		i++;
+
+	write(fd, text_content, i);
+
+	close(fd);
+	return (1);
+}
