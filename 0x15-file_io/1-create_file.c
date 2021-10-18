2b36fb156b1a8e6e0158f164f075f287a904859c
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-10-18 23:45:45 +0100

    adding 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,13 +19,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		 return (-1);
 
-	if (!text_content)
-		 write(fd, "", 0);
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
 
-	while (text_content[i])
-		i++;
-
-	write(fd, text_content, i);
+		write(fd, text_content, i);
+	}
 
 	close(fd);
 	return (1);
