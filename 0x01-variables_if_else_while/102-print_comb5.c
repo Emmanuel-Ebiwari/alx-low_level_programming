55d1d8c09f1a5f2573f299d4ba5462fb531daac1
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-06-14 08:53:13 -0700

    adding 102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,49 +1,39 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *main - print a num pair from 00-99 but no repeats
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+	int tens;
+	int ones;
+	int t;
+	int o;
 
-	while (p < 58)
+	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 	{
-		while (q < 58)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
-			while (y < 58)
+			for (t = tens; t <= '9'; t++) /*print second of pair*/
 			{
-				while (x < 58)
+				for (o = ones + 1; o <= '9'; o++)
 				{
-					putchar(p);
-					putchar(q);
+					putchar(tens);
+					putchar(ones);
 					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
+					putchar(t);
+					putchar(o);
+					if (!((tens == '9' && ones == '8') &&
+					      (t == '9' && o == '9')))
 					{
 						putchar(',');
 						putchar(' ');
 					}
-					x++;
 				}
-				y++;
-				x = 48;
+				o = '0';
 			}
-			q++;
-			q = p;
-			x = q + 1;
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
 	}
 	putchar('\n');
 	return (0);
